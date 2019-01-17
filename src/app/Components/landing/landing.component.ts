a804f14a767ade97089052df347d4faad2397273
Aravind S <saravind23399@gmail.com>
2019-01-17 21:54:40 +0530

    Changes
    Aravind S

--- src/app/Components/landing/landing.component.ts
+++ src/app/Components/landing/landing.component.ts
@@ -11,6 +11,22 @@ export class LandingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $('body').scrollspy({
+          target: ".scrollspy", 
+          offset: 50
+      });   
+  });
+  
+  $(".navbar a").on('click', function(event){
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top - 50
+    }, 800, function(){
+      window.location.hash = hash;
+    });
+  });
   }
 
 }
